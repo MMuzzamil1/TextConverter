1400c8a7857e9f529c3620ab5f34e767201ad098
MMuzzamil1 <124052423+MMuzzamil1@users.noreply.github.com>
2023-09-18 14:26:22 +0500

    Removed Imported link because we was not using it.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TextForm from "./components/TextForm";
 import About from "./components/About";
 import { useState } from "react";
 import Alert from "./components/Alert";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); // wheather dark mode is enabled or not
